dfb33b1473fc501291d5e8cdbc2671f5dededbcd
Wayne <minderrd@soarogo.com>
2021-12-25 09:29:11 +0800

    fix google eslint style

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: [
-    'google',
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'google',
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {},
 }
