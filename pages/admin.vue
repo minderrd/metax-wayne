538ff517e82c0a59dbf9843b944fb9a4a37c8585
Wayne <minderrd@soarogo.com>
2021-12-25 21:54:14 +0800

    add admin page

---
+++ pages/admin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this is admin
+
+    <v-btn @click="$store.dispatch('auth/logout');$router.push('/auth/login')">
+      Logout
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminPage',
+}
+</script>
