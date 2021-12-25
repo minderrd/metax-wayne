ca6c0c9d620d703067eefd0c5e8eec36ba713c04
Wayne <minderrd@soarogo.com>
2021-12-25 21:53:07 +0800

    add login page

---
+++ components/auth/LoginForm.vue
@@ -0,0 +1,116 @@
+<template>
+  <validation-observer
+    ref="observer"
+  >
+    <v-form @submit.prevent="login">
+      <validation-provider
+        v-slot="{errors}"
+        name="Name"
+        :rules="{ required: true, email: true }"
+      >
+        <v-text-field
+          v-model="email"
+          label="Email"
+          :error-messages="errors"
+          placeholder="Please enter your email address"
+        />
+      </validation-provider>
+
+      <validation-provider
+        v-slot="{errors}"
+        name="Password"
+        vid="password"
+        :rules="{ required: true }"
+      >
+        <v-text-field
+          v-model="password"
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          :error-messages="errors"
+          placeholder="Please enter your password"
+        >
+          <template #append>
+            <v-btn
+              small
+              icon
+              tabindex="-1"
+              @click="showPassword = !showPassword"
+            >
+              <v-icon v-if="showPassword">
+                mdi-eye-off
+              </v-icon>
+              <v-icon v-else>
+                mdi-eye
+              </v-icon>
+            </v-btn>
+          </template>
+        </v-text-field>
+      </validation-provider>
+
+      <v-btn
+        color="primary"
+        depressed
+        block
+        nuxt
+        :loading="loading"
+        @click="login"
+      >
+        Login
+      </v-btn>
+    </v-form>
+  </validation-observer>
+</template>
+
+<script>
+import { required, email } from 'vee-validate/dist/rules'
+import {
+  extend, ValidationObserver,
+  ValidationProvider, setInteractionMode,
+} from 'vee-validate'
+
+setInteractionMode('eager')
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty',
+})
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid',
+})
+
+export default {
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      showPassword: false,
+      loading: false,
+    }
+  },
+  methods: {
+    async login() {
+      this.loading = true
+      try {
+        if (!await this.$refs.observer.validate()) {
+          throw new Error('The email or password is invalid!')
+        }
+
+        await this.$store.dispatch('auth/login', {
+          email: this.email,
+          password: this.password,
+        })
+        this.$router.push('/admin')
+      } catch (e) {
+        this.$toast.error(e.message)
+      }
+      this.loading = false
+    },
+  },
+}
+</script>
---
+++ pages/auth/login.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col style="max-width: 400px">
+        <v-card>
+          <v-card-title class="headline">
+            Login
+          </v-card-title>
+          <v-card-subtitle>
+            Login to MetaX
+          </v-card-subtitle>
+          <v-card-text>
+            <AuthLoginForm />
+            <v-row no-gutters class="my-4" justify="center">
+              <nuxt-link :to="`/signup`" class="text-decoration-none">
+                Forget Password
+              </nuxt-link>
+            </v-row>
+          </v-card-text>
+          <v-card-text align="center">
+            <v-divider />
+            <span>
+              Don't have an account?
+              <nuxt-link
+                to="/auth/signup"
+                class="font-weight-bold text-decoration-none"
+              >
+                Sign Up!
+              </nuxt-link>
+            </span>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AuthLoginPage',
+  layout: 'auth',
+  asyncData({ store, redirect }) {
+    if (store.state.auth.isAuthenticated) {redirect('/admin')}
+  },
+  computed: {
+    auth() {
+      return this.$store.state.auth
+    },
+  },
+}
+</script>
