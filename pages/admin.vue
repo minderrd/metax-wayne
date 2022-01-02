0a3d8f9f0a571873a4d01e320bd57f1f056c5d6a
Wayne <minderrd@soarogo.com>
2022-01-02 16:32:49 +0800

    add admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-    this is admin
-
-    <v-btn @click="$store.dispatch('auth/logout');$router.push('/auth/login')">
-      Logout
-    </v-btn>
-  </div>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12" md="6" class="pa-2">
+        <v-card>
+          <v-card-title>
+            Dashboard
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3" class="pa-2">
+        <v-card>
+          <v-card-title>
+            Recent Login Users
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3" class="pa-2">
+        <v-card>
+          <v-card-title>
+            Dashboard
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'AdminPage',
+  layout: 'admin',
 }
 </script>
