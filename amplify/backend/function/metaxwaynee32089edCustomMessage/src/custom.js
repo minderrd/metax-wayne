fb618a2e2943d8e53186c82ed64f4e93dcd912f0
Wayne <minderrd@soarogo.com>
2022-01-03 13:46:40 +0800

    update custome message lambda function

--- amplify/backend/function/metaxwaynee32089edCustomMessage/src/custom.js
+++ amplify/backend/function/metaxwaynee32089edCustomMessage/src/custom.js
@@ -1,37 +1,45 @@
-exports.handler = (event) => {
-  // TODO implement
+/**
+ * Custome email message for the following trigger sources
+ * CustomMessage_SignUp, CustomMessage_ResendCode, CustomMessage_ForgotPassword
+ * @param {*} event
+ * @returns
+ */
 
-  const messageSources = ['CustomMessage_SignUp', 'CustomMessage_ResendCode']
-  if (!messageSources.includes(event.triggerSource)) return event
+exports.handler = (event) => {
+  const signupSources = ['CustomMessage_SignUp', 'CustomMessage_ResendCode']
+  const forgetSources = ['CustomMessage_ForgotPassword']
+
+  if (
+    !signupSources.includes(event.triggerSource) &&
+    !forgetSources.includes(event.triggerSource)
+  ) return event
 
   const { codeParameter } = event.request
   const { userName, region } = event
   const { clientId } = event.callerContext
   const { email } = event.request.userAttributes
-  const { BASE_URL } = process.env
-  const link = `<a href="${BASE_URL}/auth/signupConfirm?code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${email}" target="_blank">here</a>`
-  event.response.emailSubject = '[Meta X Wayne]Your verification link' // request.codeParameter
-  event.response.emailMessage = `Thank you for signing up. Click ${link} to verify your email.`
+  const BASE_URL = process.env.BASE_URL || 'https://d1073yf0ptkwai.cloudfront.net'
 
-  console.log(event)
+  if (signupSources.includes(event.triggerSource)) {
+    // Sign up email message
+    let link = `<a href="${BASE_URL}/auth/signupConfirm?`
+    link += `code=${codeParameter}&username=${userName}&`
+    link += `clientId=${clientId}&region=${region}&email=${email}" `
+    link += `target="_blank">here</a>`
+    event.response.emailSubject = '[Meta X Wayne]Your verification link'
+    event.response.emailMessage = `Thank you for signing up. `
+    event.response.emailMessage += `Click ${link} to verify your email.`
+  } else if (forgetSources.includes(event.triggerSource)) {
+    // forget password email message
+    let link = `<a href="${BASE_URL}/auth/forgetConfirm?`
+    link += `code=${codeParameter}&username=${userName}&`
+    link += `clientId=${clientId}&region=${region}&email=${email}" `
+    link += `target="_blank">here</a>`
+    event.response.emailSubject = '[Meta X Wayne]Your reset password link'
+    event.response.emailMessage = `Thank you for using my website. `
+    event.response.emailMessage += `Click ${link} to reset your password.`
+  }
+
   return event
 }
 
-
-// exports.handler = (event, context, callback) => {
-//   // Identify why was this function invoked
-//   if (event.triggerSource === 'CustomMessage_SignUp' || event.triggerSource === 'CustomMessage_ResendCode') {
-//     // Ensure that your message contains event.request.codeParameter. This is the placeholder for code that will be sent
-//     const { codeParameter } = event.request
-//     const { userName, region } = event
-//     const { clientId } = event.callerContext
-//     const { email } = event.request.userAttributes
-//     const { API_BASE_URL } = process.env
-//     const link = `<a href="${API_BASE_URL}/auth/signup/confirm?code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${email}" target="_blank">here</a>`
-//     event.response.emailSubject = '[Meta X Wayne]Your verification link' // event.request.codeParameter
-//     event.response.emailMessage = `Thank you for signing up. Click ${link} to verify your email.`
-//   }
-
-//   // Return to Amazon Cognito
-//   callback(null, event)
-// }
