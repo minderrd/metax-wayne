0d59a48863ac6046f599e2321a4e3dc74a3cbe0a
Wayne <minderrd@soarogo.com>
2022-01-03 22:12:18 +0800

    update admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,8 +4,26 @@
       <v-col cols="12" md="6" class="pa-2">
         <v-card>
           <v-card-title>
-            Dashboard
+            User List
           </v-card-title>
+          <v-simple-table>
+            <template #default>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                <template v-for="user of users">
+                  <tr :key="`user-uuid-${user.sub}`">
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.email }}</td>
+                  </tr>
+                </template>
+              </tbody>
+            </template>
+          </v-simple-table>
         </v-card>
       </v-col>
       <v-col cols="12" md="3" class="pa-2">
@@ -30,5 +48,27 @@
 export default {
   name: 'AdminPage',
   layout: 'admin',
+  data() {
+    return {
+      users: [],
+    }
+  },
+  computed: {
+    auth() {
+      return this.$store.state.auth
+    },
+  },
+  async created() {
+    await this.getUsers()
+  },
+  methods: {
+    async getUsers() {
+      try {
+        this.users = await this.$store.dispatch('users/getUsers')
+      } catch (e) {
+        this.$toast.error(e.message)
+      }
+    },
+  },
 }
 </script>
