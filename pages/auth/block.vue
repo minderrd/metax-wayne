bdcbd4d5ce1acafa1db4583296c6adb592f5c635
Wayne <minderrd@soarogo.com>
2022-01-18 22:10:43 +0800

    add block page

---
+++ pages/auth/block.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col style="max-width: 400px">
+        <v-card>
+          <v-card-title>
+            You haven't verify your email address
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AuthBlockPage',
+  layout: 'auth',
+}
+</script>
